ce9af9ef07c2362057d84f224580fa8fa16cdd9b
Sage Betko <sage.betko@gmail.com>
2024-12-03 17:20:25 -0500

    Fix streamlit run command

--- run_with_tests.py
+++ run_with_tests.py
@@ -8,11 +8,13 @@ def setup_test_environment():
     test_apps_dir = os.path.join(pkg_dir, "tests", "apps")
 
     # Copy test apps and apps.yml to the current directory
-    shutil.copytree(test_apps_dir, "./apps", dirs_exist_ok=True)
+    shutil.copytree(test_apps_dir, ".", dirs_exist_ok=True)
 
 
 def run_manager():
-    subprocess.run(["python", "-m", "app_manager.manager"])
+    pkg_dir = os.path.dirname(__file__)
+    manager_path = os.path.join(pkg_dir, "src", "app_manager", "manager.py")
+    subprocess.run(["python", "-m", "streamlit", "run", manager_path])
 
 
 if __name__ == "__main__":
